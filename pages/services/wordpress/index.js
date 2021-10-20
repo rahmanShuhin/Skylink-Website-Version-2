2265e2a15d5e366a5cb50bf6d547d2d0ef6ac8e2
Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2021-10-20 15:43:29 +0600

    top banner done

--- pages/services/wordpress/index.js
+++ pages/services/wordpress/index.js
@@ -1,10 +1,18 @@
+import Head from "next/head";
+import Wordpress from "../../../components/WordPress/Wordpress";
 const index = () => {
     return (
         <div>
-            {/* for rafi */}
-            <h2>i am working on it</h2>
+            <Head>
+                <title>SkyLink Creative | Web development Agency</title>
+                <meta name="description" content="SkyLink Creative" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <Wordpress/>
+            </main>
         </div>
     );
-};
+};  
 
 export default index;
