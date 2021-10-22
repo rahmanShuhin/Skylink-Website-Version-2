8ecf6ad072de31839b869f9afa5d756fde76af48
Dibyo178 <Sourov.mu@gmail.com>
2021-10-22 22:23:15 +0600

    footer-update

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,4 +1,9 @@
+
 import React from 'react';
+import { AiFillFacebook, AiFillTwitterSquare, AiOutlineMail } from 'react-icons/ai';
+import { BsInstagram } from 'react-icons/bs';
+import { FaMapMarkerAlt, FaPhoneAlt, FaYoutubeSquare } from 'react-icons/fa';
+
 
 const Footer = () => {
   return (
@@ -6,16 +11,26 @@ const Footer = () => {
       <div className="footer_item">
         <h2>About us</h2>
         <p>Skylink creative success is attributed to the collective performance of its team. Effective, open communication in our workplace keeps us focused and driven towards providing you with an outstanding service.</p>
+        <ul className="footer_list">
+          {/* <a href=""></a> */}
+          <li> <a href=""><AiFillFacebook size={25}  /></a> </li>
+          <li> <a href=""> <AiFillTwitterSquare size={25}  /></a> </li>
+          <li> <a href=""> <BsInstagram size={25}  /></a> </li>
+          <li> <a href=""><FaYoutubeSquare size={25}  /></a></li>
+         
+      </ul>
       </div>
       <div className="footer_item">
-        <h2>Skylink creative</h2>
+      <h2>Quick Links</h2>
+        {/* <h2>Skylink creative</h2> */}
         <div className="footer_link">
-          <div>
+          {/* <div>
             <p>Sylhet 3100, Bangladesh</p>
             <p>contact@skylinkcreative.com</p>
             <p>Cell No</p>
-          </div>
+          </div> */}
           <div className="footer_link_items">
+            
             <p>Services</p>
             <p>Portfolio</p>
             <p>Terms & condition</p>
@@ -31,20 +46,35 @@ const Footer = () => {
         <p>Privacy policy</p>
       </div> */}
       <div className="footer_item">
-        <h2>Join Newsletter</h2>
-        <form>
-          <input
-            required
-            name="email"
-            type="text"
-            className="footer_input"
-            placeholder="Enter Your Email Address"
-          />
-          <button type="submit" className="btn">
-            Subscribe
-          </button>
-        </form>
+        <h2>Contact Info</h2>
+        <ul className="footer_info">
+          <li>
+            
+            <p><FaMapMarkerAlt size={20}/>  Sylhet 3100, Bangladesh</p>
+          </li>
+         
+          <li>
+            
+            <p> <FaPhoneAlt size={20}/>   Cell No:+14465676676</p>
+          </li>
+
+          <li>
+            <p><AiOutlineMail size={20}/>  contact@skylinkcreative.com</p>
+          </li>
+
+        </ul>
       </div>
+
+      
+   
+        {/* <p style={{ textAlign: 'center', color: 'white',marginLeft:'600px',fontsize:'10px' }}>
+          Sylhet 3100, Bangladesh</p>
+        <br/>
+        <p>contact@skylinkcreative.com</p>
+        <br/>
+            <p>Cell No</p> */}
+          
+        
     </div>
   );
 };
